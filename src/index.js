7363de006c91a263a90b84d04dd4f64676c1eca4
tingluhk <tingluhk@gmail.com>
2018-03-23 22:16:43 +0800

    line spacing in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 // In react if you wwant to do the same thing, first need to create a react element of type h1 with content 'hello world' 2
-var App = React.createElement('hi',null,'hello world');
+var App = React.createElement('h1',null,'hello world');
 
 // take react element 'App' and display it on page on DOM.
-ReactDOM.render(App);
+// ReactDom.render([what],[where])
+ReactDOM.render(
+  App,
+  document.getElementById('root')
+);
