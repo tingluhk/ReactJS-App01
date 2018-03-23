7cf79bdeb65f53e001e24f64463957f256c0f369
tingluhk <tingluhk@gmail.com>
2018-03-23 21:12:49 +0800

    changed index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+// In react if you wwant to do the same thing, first need to create a react element of type h1 with content 'hello world'
+
